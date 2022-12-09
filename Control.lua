a4a44e578e4527fe0a0f02a703826edc951f4475
untypeable <72481356+untypeable@users.noreply.github.com>
2022-12-08 18:08:22 -0600

    Update Control.lua

--- Control.lua
+++ Control.lua
@@ -1,59 +1,56 @@
-local PlayerService = game:GetService("Players")
-local GUI = PlayerService.LocalPlayer.PlayerGui:WaitForChild("SOA")
-
-local PrimaryColor = Color3.new(50 / 255, 50 / 255, 50 / 255)
-local SecondaryColor = Color3.new(75 / 255, 75 / 255, 75 / 255)
-
-local function MonitorTargetPlayer(Player)
-	local sUserId = tostring(Player.UserId)
-	local Velocity
-	local UserPos
-	while GUI.Main.Players.UserId.TextBox.Text == sUserId do
-		if Player.Character ~= nil then
-			if Player.Character.HumanoidRootPart ~= nil then
-				Velocity = math.floor(Player.Character.HumanoidRootPart.Velocity.magnitude)
-				UserPos = Player.Character.HumanoidRootPart.Position
-				GUI.Main.Players.Velocity.TextBox.Text = tostring(Velocity)
-				GUI.Main.Players.UserPos.TextBox.Text = tostring(math.floor(UserPos.X)) .. ", " .. tostring(math.floor(UserPos.Y)) .. ", " .. tostring(math.floor(UserPos.Z))
-			end
-		end
-		wait(0.1)
-	end
-end
-
-local function ToggleTargetPlayer(Player)
-	for _, Entry in pairs(GUI.Main.Players.PlayerList:GetChildren()) do
-		if Entry:IsA("TextButton") then
-			if Entry.Text ~= "  "..Player.Name then
-				Entry.BackgroundColor3 = SecondaryColor
-			else
-				Entry.BackgroundColor3 = PrimaryColor
+local function NavButtonSelect(NavButton)
+	for _, Button in pairs(Fartware.Main.Nav:GetChildren()) do
+		if Button:IsA("TextButton") and Button ~= NavButton then
+			Button.BackgroundColor3 = Color3.new(25 / 255, 25 / 255, 25 / 255)
+			if Fartware.Main:FindFirstChild(Button.Text) then
+				Fartware.Main[Button.Text].Visible = false
 			end
 		end
 	end
-	coroutine.resume(coroutine.create(function()
-		local Image, Ready = PlayerService:GetUserThumbnailAsync(Player.UserId, Enum.ThumbnailType.AvatarThumbnail, Enum.ThumbnailSize.Size420x420)
-		GUI.Main.Players.AvatarImg.Image = Image
-	end))
-	GUI.Main.Players.Username.TextBox.Text = Player.Name
-	GUI.Main.Players.UserId.TextBox.Text = tostring(Player.UserId)
-	GUI.Main.Players.Nickname.TextBox.Text = Player.DisplayName
-	coroutine.resume(coroutine.create(MonitorTargetPlayer(Player)))
+	NavButton.BackgroundColor3 = Color3.new(50 / 255, 50 / 255, 50 / 255)
+	if Fartware.Main:FindFirstChild(NavButton.Text) then
+		Fartware.Main[NavButton.Text].Visible = true
+	end
 end
 
-local function RefreshPlayerList()
-	local Y = 0
-	for _, Player in pairs(PlayerService:GetPlayers()) do
-		local GuiEntry = GUI.Main.Players.PlayerList.PlayerEntry:Clone()
-		GuiEntry.Position = UDim2.new(0, Y, 0, 0)
-		GuiEntry.Text = "  "..Player.Name
-		GuiEntry.Visible = true
-		GuiEntry.Parent = GUI.Main.Players.PlayerList
-		GuiEntry.MouseButton1Click:Connect(function()
-			ToggleTargetPlayer(Player)
+local function ExploitToggle(ToggleFrame)
+	if ToggleFrame.TextLabel.Text == "N" then
+		ToggleFrame.TextLabel.Text = "Y"
+		ToggleFrame.TextLabel.TextColor3 = Color3.new(150 / 255, 255 / 255, 150 / 255)
+	else
+		ToggleFrame.TextLabel.Text = "N"
+		ToggleFrame.TextLabel.TextColor3 = Color3.new(255 / 255, 100 / 255, 100 / 255)
+	end
+end
+
+Fartware.Main.ExitButton.MouseButton1Click:Connect(function()
+	Fartware:Destroy()
+end)
+
+Fartware.Main.MinButton.MouseButton1Click:Connect(function()
+	if Fartware.Main.Size.Width.Offset == 800 then
+		Fartware.Main.Size = UDim2.new(0, 400, 0, 20)
+		Fartware.Main.Home.Visible = false
+		Fartware.Main.Nav.Visible = false
+	else
+		Fartware.Main.Size = UDim2.new(0, 800, 0, 400)
+		Fartware.Main.Nav.Visible = true
+		Fartware.Main.Home.Visible = true
+	end
+end)
+
+for _, Button in pairs(Fartware.Main.Nav:GetChildren()) do
+	if Button:IsA("TextButton") then
+		Button.MouseButton1Click:Connect(function()
+			NavButtonSelect(Button)
 		end)
-		Y += 30
 	end
 end
 
-RefreshPlayerList()
+for _, Toggle in pairs(Fartware.Main.Home.Toggles:GetChildren()) do
+	if Toggle:IsA("Frame") then
+		Toggle.TextButton.MouseButton1Click:Connect(function()
+			ExploitToggle(Toggle)
+		end)
+	end
+end
